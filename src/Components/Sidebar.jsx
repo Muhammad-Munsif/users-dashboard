5fd9ba54233580e944b33155445e676d8840f5ea
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-02 10:49:40 +0500

    small changes in sidebar component to designed for scrollbar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -49,7 +49,7 @@ const Sidebar = () => {
       {/* Sidebar */}
       <div
         className={`fixed top-0 left-0 h-full w-64 bg-white shadow-lg p-6 transform transition-transform duration-300 z-40 
-        ${open ? "translate-x-0" : "-translate-x-full"} md:translate-x-0`}
+        ${open ? "translate-x-0" : "-translate-x-full"} md:translate-x-0 overflow-y-auto scrollbar-thin scrollbar-thumb-pink-500 scrollbar-track-pink-100`}
       >
         <div className="flex items-center gap-3 mb-6">
           <div className="size-8 rounded-lg bg-indigo-600" />
